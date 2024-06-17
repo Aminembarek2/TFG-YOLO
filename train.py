c936e595be7375bd2524f4f1fe609639c0d755d7
aminembarek2 <62722834+Aminembarek2@users.noreply.github.com>
2024-06-17 20:11:00 +0200

    The train/test lists

--- train.py
+++ train.py
@@ -4,4 +4,4 @@ import os
 # Load a pretrained YOLO model
 model = YOLO('yolov8x.pt')
 
-model.train(data='/home/kitti/data.yaml', project="models",epochs=40, save=True, imgsz=640, device='0', val=False, save_period=10, lrf=0.0001, lr0=0.0001, batch=32)
+model.train(data='/home/kitti/data.yaml', project="models",epochs=40, save=True, imgsz=640, device='0', val=False, mosaic=0.0,save_period=10, lrf=0.0001, lr0=0.0001, batch=32)
